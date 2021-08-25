bc39b2997ec4ee6d6fbd0646908c781bb8be25a1
Tomas Assenza <tomyassenza@gmail.com>
2021-08-25 05:02:32 -0300

    Interfaz detectando archivos y mostrandolos en lista

--- simae/src/main/java/simae/gui/SelectorApplication.java
+++ simae/src/main/java/simae/gui/SelectorApplication.java
@@ -1,5 +1,27 @@
 package simae.gui;
 
-public class SelectorApplication {
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class SelectorApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("selector.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- simae/src/main/java/simae/gui/SelectorApplicationController.java
+++ simae/src/main/java/simae/gui/SelectorApplicationController.java
@@ -1,8 +1,28 @@
 package simae.gui;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
 
 
 public class SelectorApplicationController {
 
+    @FXML
+    private ListView<?> listaDeArchivos;
+
+    private ObservableList l = FXCollections.observableArrayList();
+
+    @FXML
+    void multiFileChooser() {
+        FileChooser fc = new FileChooser();
+        List<File> archivos = fc.showOpenMultipleDialog(null);
+        archivos.forEach(file -> l.add(file));
+        listaDeArchivos.setItems(l);
+    }
 }
